c49be42d439ecbf450ca36b4686f38c5add71031
Rachel Katz <rachel.katz@flatironschool.com>
2022-04-05 14:29:14 -0400

    commit tree 0e05a3ab1ef43a3b0ca448c6ad0dcab27fbfeff3

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+let superbowlWin = (arr) => {
+    let year = arr.find(el => el.result === "W")
+    if(year) {
+        return year.year
+    } else {
+        return undefined
+    }
+}
